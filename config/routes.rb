de82538f08099688f57c57c9cd852a9e532912b6
Philippe Ka <ruby@knafo.fr>
2017-11-06 17:53:47 +0100

    modifying the routes, ready to merge with master

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-
-  get 'bookings/create'
-
-  get 'bookings/update'
-
-  get 'bookings/edit'
-
-  get 'bookings/destroy'
-
-  get 'bookings/index'
-
-  get 'bookings/show'
-
   devise_for :users
+
   root to: 'pages#home'
-  resources :destinations do
+
+  resources :destinations, only: [:create, :index, :destroy] do
     resources :bookings
   end
 end
