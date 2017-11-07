c0c3ca2967c12468d8791b402ac41372693e3a78
Stephan De Grove <stephandegrove@gmail.com>
2017-11-07 11:35:13 +0100

    6 destination added to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+type_to_sample = ["moon","planet"]
+name_to_sample = ["Earth","Neptune", "Mercure", "Mars", "Saturne", "Venus"]
+distance_to_randomize = (100000..50000000).to_a
+price_to_randomize = (500..10000).to_a
+
+
+puts "Seeding Destinations"
+Destination.destroy_all
+puts "Adding 7 Destinations "
+name_to_sample.each do |planet_name|
+  destination = Destination.new(name: planet_name, planet_type: type_to_sample.sample, distance: distance_to_randomize.sample, price: price_to_randomize.sample )
+  destination.save
+puts "destination created #{destination.name }"
+end
