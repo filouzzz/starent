7061138d5a4dea2dc5a6b29680bf70276337a4d4
Stephan De Grove <stephandegrove@gmail.com>
2017-11-08 18:36:19 +0100

    Origin Fonctional

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,27 +3,17 @@ class DestinationsController < ApplicationController
 
   def index
    @destinations = Destination.all
- end
-
+  end
 
  def show
 
  end
 
+ # def destination_booked
+ #  @destination.booked = !@destination.booked
+ #  end
 
-
- def new
-  @destination = Destination.find(params[set_destination])
-end
-
-def show
-end
-
-
-
-end
-
-def display_unbooked_destinations
+ def display_unbooked_destinations
   @destinations = Destination.where(booked: false)
 end
 
@@ -36,36 +26,30 @@ def new
   @destination = Destination.new
 end
 
-
 def create
  user_id = current_user
  @destination = Destination.new(destination_params)
  @destination.user_id = user_id
- if @destination.save
-  redirect_to destination_path
-else
-  render :new
-end
+ @destination.save
 
-end
+    # no need for app/views/destinations/create.html.erb
+    redirect_to destination_path(@destination)
+  end
 
-def update
- respond_to do |format|
-  if @destination.update(destination_params)
-    redirect_to @destination
-  else
-    render :edit
+  def update
+    @destination.update(destination_params)
+
+    # no need for app/views/destinations/update.html.erb
+    redirect_to destination_path(@destination)
+  end
+
+  private
+
+  def set_destination
+    @destination = Destination.find(params[:id])
+  end
+  def destination_params
+    params.require(:destination).permit(:name, :distance)
   end
 end
 
-private
-
-def set_destination
-  @destination = Destination.find(params[:id])
-end
-def destination_params
-  params.require(:destination).permit(:name, :distance, :planet_type, :price, :weather)
-end
-end
-
-end
