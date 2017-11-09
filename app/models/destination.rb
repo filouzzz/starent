789cd85c1a90a0b7db995949cf02a39ccde31ae6
hasna Oulad <ouladch@gmail.com>
2017-11-09 11:34:25 -0500

    ajout du search by name

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,4 +2,8 @@ class Destination < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, uniqueness: true, presence: true
+
+  def self.search(search)
+    where("name ILIKE ? OR weather ILIKE ?", "%#{search}%", "%#{search}%")
+  end
 end
